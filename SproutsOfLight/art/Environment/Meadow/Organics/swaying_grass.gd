29cf300a4e32634fe41a1372630222f16417d666
shawndinhstephens <shawndinh25@gmail.com>
2026-04-11 17:03:40 -0700

    swaying grass

--- SproutsOfLight/art/Environment/Meadow/Organics/swaying_grass.gd
+++ SproutsOfLight/art/Environment/Meadow/Organics/swaying_grass.gd
@@ -1,15 +1,35 @@
 extends Area2D
 
+@onready var sprite_2d: Sprite2D = $Sprite2D
 
-# Called when the node enters the scene tree for the first time.
+@export var skewValue := 15
+@export var windSpeed := 2.0
+@export var windStrength := 0.6
+@export var bendGrassAnimationSpeed = 0.3
+@export var grassReturnAnimationSpeed = 5.0
+
+var _wind_tween: Tween
 func _ready() -> void:
-	pass # Replace with function body.
-
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	pass
-
-
-func _on_body_shape_entered(body_rid: RID, body: Node2D, body_shape_index: int, local_shape_index: int) -> void:
-	pass # Replace with function body.
+	_start_wind()
+	
+func _start_wind() -> void:
+	_wind_tween = create_tween().set_loops()
+	
+	var peak := skewValue * windStrength
+	var half := windSpeed / 2.0
+	
+	#Sway right
+	_wind_tween.tween_property(
+		sprite_2d.material,
+		"shader_parameter/skew",
+		peak,
+		half
+	).set_ease(Tween.EASE_IN_OUT).set_trans(Tween.TRANS_SINE)
+	
+	#Sway right
+	_wind_tween.tween_property(
+		sprite_2d.material,
+		"shader_parameter/skew",
+		-peak,
+		half
+	).set_ease(Tween.EASE_IN_OUT).set_trans(Tween.TRANS_SINE)
